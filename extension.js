8cd179cfe6fea725016be075a756dcd07af6962d
domgregori <dom.gregori@gmail.com>
2025-08-01 02:59:12 -0400

    Implemented overlay dimming

--- extension.js
+++ extension.js
@@ -21,36 +21,51 @@ import St from 'gi://St';
 
 import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
-import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 const Indicator = GObject.registerClass(
 class Indicator extends PanelMenu.Button {
     _init() {
-        super._init(0.0, _('My Shiny Indicator'));
+        super._init(0.0, _('Toggle Dim Overlay'));
 
         this.add_child(new St.Icon({
-            icon_name: 'face-smile-symbolic',
+            icon_name: 'lightbulb-symbolic',
             style_class: 'system-status-icon',
         }));
-
-        let item = new PopupMenu.PopupMenuItem(_('Show Notification'));
-        item.connect('activate', () => {
-            Main.notify(_('Whatʼs up, folks?'));
-        });
-        this.menu.addMenuItem(item);
     }
 });
 
-export default class IndicatorExampleExtension extends Extension {
+export default class OverlayDimmerExtension extends Extension {
     enable() {
         this._indicator = new Indicator();
         Main.panel.addToStatusArea(this.uuid, this._indicator);
+
+        const primaryMonitor = Main.layoutManager.primaryMonitor;
+
+        this._overlay = new St.Bin({
+            style_class: 'dim-overlay',
+            reactive: false,
+            can_focus: false,
+            track_hover: false,
+            width: primaryMonitor.width,
+            height: primaryMonitor.height,
+            visible: false,
+        });
+        Main.uiGroup.add_child(this._overlay);
+
+        this._indicator.connect('button-press-event', () => {
+            this._overlay.visible = !this._overlay.visible;
+        });
     }
 
     disable() {
         this._indicator.destroy();
         this._indicator = null;
+
+        if (this._overlay) {
+            this._overlay.destroy();
+            this._overlay = null;
+        }
     }
 }

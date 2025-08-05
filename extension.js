1823f7b1ef059481fd868033d9ad81262b36ef1e
domgregori <dom.gregori@gmail.com>
2025-08-04 21:49:27 -0400

    Error loading schema

--- extension.js
+++ extension.js
@@ -16,82 +16,118 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
-import GObject from "gi://GObject";
-import St from "gi://St";
-import Gio from "gi://Gio";
 
-import * as PanelMenu from "resource:///org/gnome/shell/ui/panelMenu.js";
-import * as Main from "resource:///org/gnome/shell/ui/main.js";
+import St from 'gi://St';
+import Gio from 'gi://Gio';
+import Clutter from 'gi://Clutter';
+import { Slider } from 'resource:///org/gnome/shell/ui/slider.js';
 
-// import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
-import {
-  Extension,
-  gettext as _
-} from "resource:///org/gnome/shell/extensions/extension.js";
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
+import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
 export default class OverlayDimmerExtension extends Extension {
-  constructor(metadata) {
-    super(metadata);
-  }
-
-  enable() {
-    this._indicator = new PanelMenu.Button(this.metadata.name);
-    const icon = new St.Icon({
-      style_class: "system-status-icon overlaydimmer-icon"
-    });
-    icon.gicon = Gio.icon_new_for_string(
-      this.path + "/icons/overlaydimmer.symbolic.png"
-    );
-    this._indicator.add_child(icon);
-
-    Main.panel.addToStatusArea(this.uuid, this._indicator);
-    this._tapHandler = this._indicator.connect(
-      "touch-event",
-      this._buttonActivated.bind(this)
-    );
-    this._clickHandler = this._indicator.connect(
-      "button-press-event",
-      this._buttonActivated.bind(this)
-    );
-  }
-
-  disable() {
-    if (this._tapHandler) {
-      this._indicator.disconnect(this._tapHandler);
-      this._tapHandler = null;
-    }
-    if (this._clickHandler) {
-      this._indicator.disconnect(this._clickHandler);
-      this._clickHandler = null;
-    }
-
-    if (this._overlay) {
-      this._overlay.destroy();
-      this._overlay = null;
+    enable() {
+        this._settings = new Gio.Settings({schema_id: 'org.gnome.shell.extensions.overlaydimmer'});
+        this._overlayOpacity = this._settings.get_double('dim-level');
+        this._indicator = new PanelMenu.Button(this.metadata.name);
+        const icon = new St.Icon({
+            style_class: 'system-status-icon overlaydimmer-icon',
+        });
+        icon.gicon = Gio.icon_new_for_string(
+            `${this.path}/icons/overlaydimmer.symbolic.png`
+        );
+        this._indicator.add_child(icon);
+        // this._tapHandler = this._indicator.connect(
+        //     'touch-event',
+        //     this._buttonActivated.bind(this)
+        // );
+        this._clickHandler = this._indicator.connect('button-press-event', (actor, event) => {
+            const button = event.get_button();
+            if (button === Clutter.BUTTON_SECONDARY) {
+                this.showSliderPopup();
+                return Clutter.EVENT_STOP; // suppress default menu
+            }
+            return Clutter.EVENT_PROPAGATE;
+        });
+        this._settingsChangedHandler = this._settings.connect('changed::dim-level', () => {
+            this._overlayOpacity = this._settings.get_double('dim-level');
+            if (this._overlay) {
+                this._overlay.set_style(
+                    `background-color: rgba(0, 0, 0, ${this._overlayOpacity});`
+                );
+            }
+        });
+        Main.panel.addToStatusArea(this.uuid, this._indicator);
     }
 
-    if (this._indicator) {
-      this._indicator.destroy();
-      this._indicator = null;
+    disable() {
+        this._indicator?.disconnect(this._tapHandler);
+        this._tapHandler = null;
+        this._indicator?.disconnect(this._clickHandler);
+        this._clickHandler = null;
+        this._overlay?.destroy();
+        this._overlay = null;
+        if (this._settings) {
+            this._settings.disconnect(this._settingsChangedHandler);
+            this._settings = null;
+        }
+        this._indicator?.destroy();
+        this._indicator = null;
     }
-  }
 
-  _buttonActivated() {
-    if (this._overlay) {
-      this._overlay.visible = !this._overlay.visible;
-    } else {
-      const primaryMonitor = Main.layoutManager.primaryMonitor;
-      this._overlay = new St.Bin({
-        style_class: "dim-overlay",
-        reactive: false,
-        can_focus: false,
-        track_hover: false,
-        width: primaryMonitor.width,
-        height: primaryMonitor.height,
-        visible: true
-      });
+    showSliderPopup() {
+        if (this._popup)
+            this._popup.destroy();
 
-      Main.uiGroup.add_child(this._overlay);
+        const currentValue = this.settings.get_int('dim-level');
+
+        this._popup = new St.Bin({
+            style_class: 'popup-slider-container',
+            reactive: true,
+            x_expand: true,
+            y_expand: true,
+            child: new Slider(currentValue),
+        });
+
+        this._popup.child.connect('value-changed', slider => {
+            const level = Math.round(slider.value * 100);
+            this.settings.set_int('dimmer-level', level);
+        });
+
+        // Show popup near the button
+        Main.uiGroup.add_actor(this._popup);
+        this._popup.set_position(
+            this._button.get_allocation_box().x1,
+            this._button.get_allocation_box().y2
+        );
+
+        // Auto-dismiss when clicking outside
+        this._popup.connect('button-press-event', () => {
+            this._popup.destroy();
+            this._popup = null;
+        });
     }
-  }
+
+    _buttonActivated(button) {
+        if (this._overlay) {
+            this._overlay.visible = !this._overlay.visible;
+        } else {
+            const primaryMonitor = Main.layoutManager.primaryMonitor;
+            this._overlay = new St.Bin({
+                style_class: 'dim-overlay',
+                reactive: false,
+                can_focus: false,
+                track_hover: false,
+                width: primaryMonitor.width,
+                height: primaryMonitor.height,
+                visible: true,
+            });
+            this._overlay.set_style(
+                `background-color: rgba(0, 0, 0, ${this._overlayOpacity});`
+            );
+            Main.uiGroup.add_child(this._overlay);
+        }
+    }
 }
+
--- prefs.js
+++ prefs.js
@@ -1,33 +1,46 @@
-
-import Adw from "gi://Adw";
-import Gtk from "gi://Gtk";
-import { ExtensionPreferences } from "resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js";
+import Adw from 'gi://Adw';
+import Gtk from 'gi://Gtk';
+import {ExtensionPreferences, gettext as _} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
 export default class OverlayDimmerPreferences extends ExtensionPreferences {
-  fillPreferencesWindow(window) {
-    const page = new Adw.PreferencesPage();
-    const group = new Adw.PreferencesGroup({
-      title: "Dimness Settings",
-    });
-    const row = new Adw.ActionRow({
-      title: "Dim Level",
-    });
+    fillPreferencesWindow(window) {
+        let settings = this.getSettings();
+        const page = new Adw.PreferencesPage();
+        window.add(page);
+        const group = new Adw.PreferencesGroup({
+            title: 'Overlay Settings',
+        });
+        const row = new Adw.ActionRow({
+            title: 'Opacity Level',
+        });
 
-    const scale = Gtk.Scale.new_with_range(
-      Gtk.Orientation.HORIZONTAL,
-      0.1,
-      1.0,
-      0.1
-    );
-    scale.set_digits(2);
-    scale.set_value(this.getSettings().get_double("dim-level"));
-    scale.connect("value-changed", () => {
-      this.getSettings().set_double("dim-level", scale.get_value());
-    });
+        const valueLabel = new Gtk.Label({
+            label: `${Math.round(settings.get_double('dim-level') * 100)}`,
+            halign: Gtk.Align.CENTER,
+        });
 
-    row.add_suffix(scale);
-    group.add(row);
-    page.add(group);
-    window.add(page);
-  }
+        const scale = new Gtk.Scale({
+            orientation: Gtk.Orientation.HORIZONTAL,
+            digits: 2,
+            adjustment: new Gtk.Adjustment({
+                lower: 0.1,
+                upper: .9,
+                step_increment: 0.05,
+            }),
+            hexpand: true,
+            draw_value: false,
+        });
+        scale.set_digits(2);
+        scale.set_value(settings.get_double('dim-level'));
+        scale.connect('value-changed', () => {
+            settings.set_double('dim-level', scale.get_value());
+            const value = scale.get_value();
+            valueLabel.set_label(`${Math.round(value * 100)}`);
+        });
+
+        row.add_suffix(valueLabel);
+        row.add_suffix(scale);
+        group.add(row);
+        page.add(group);
+    }
 }
